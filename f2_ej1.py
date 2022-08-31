17abfcc1b3fdd41f4fb409d13b4c5cfe59d80f93
Emanuel Guaraz <guarazjemanuel@gmail.com>
2022-08-31 20:31:06 -0300

    Resolución ejercicio 1 f2

--- f2_ej1.py
+++ f2_ej1.py
@@ -1,1 +1,15 @@
-""" Dado el valor de los tres lados de un triángulo, calcular el perímetro del triángulo.""""
+#Dado el valor de los tres lados de un triángulo, calcular el perímetro del triángulo.
+
+#Variables de los lados del triangulo
+l1 = l2 = l3 = 0
+
+#carga valores por teclado
+l1 = float(input("Ingrese lado 1:"))
+l2 = float(input("Ingrese lado 2:"))
+l3 = float(input("Ingrese lado 3:"))
+
+#calcula perimetro
+perimetro = l1 + l2 + l3
+
+#muetra resultado
+print("El perimetro del triangulo es", perimetro)
